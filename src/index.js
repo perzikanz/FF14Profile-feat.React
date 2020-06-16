a8354dcda56e6eb2867884d317d819566c058f37
perzikanz <65535030+perzikanz@users.noreply.github.com>
2020-06-16 14:22:17 +0900

    ボタンを追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import Form from "./js/components/Form";
+import React from 'react';
+import ReactDOM, { render } from "react-dom";
+
+
+function Button() {
+  return(
+    <button onClick={() => console.log("押されました")}>
+      押してね
+    </button>
+  );
+}
+
+
+
+ReactDOM.render(
+  <Button />,
+  document.getElementById('root')
+);
