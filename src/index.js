3a16da44c9324c1f66d6da334ca21b7c73e0d868
perzikanz <65535030+perzikanz@users.noreply.github.com>
2020-06-17 13:59:51 +0900

    Formを追加

--- src/index.js
+++ src/index.js
@@ -2,17 +2,45 @@ import React from 'react';
 import ReactDOM, { render } from "react-dom";
 
 
-function Button() {
+function Button(props) {
   return(
-    <button onClick={() => console.log("押されました")}>
+    <button onClick={() => getCharaData(props.value)}>
       押してね
     </button>
   );
 }
 
+class Form extends React.Component {
+  constructor(){
+    super();
+    this.state = {value: ""};
 
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  render(){
+    return (
+      <div>
+        <label>
+          Lodestone ID：
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <Button value={this.state.value} />
+      </div>
+    );   
+  }
+}
+
+
+function getCharaData(id){ 
+  fetch(`https://xivapi.com/character/${id}?data=CJ`).then(response => response.json()).then(jsonData => console.log(jsonData));
+}
 
 ReactDOM.render(
-  <Button />,
+  <Form />,
   document.getElementById('root')
 );
