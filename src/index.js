4c1fbe9665ce308911ef2cee593749c35b4c2233
perzikanz <65535030+perzikanz@users.noreply.github.com>
2020-06-18 17:03:38 +0900

    キャラ名、キャラ画像の反映

--- src/index.js
+++ src/index.js
@@ -1,46 +1,89 @@
 import React from 'react';
 import ReactDOM, { render } from "react-dom";
+import './index.css';
 
 
 function Button(props) {
   return(
-    <button onClick={() => getCharaData(props.value)}>
+    <button onClick={() => getCharaData(props.lodestoneId, props.setCharacterData)}>
       押してね
     </button>
   );
 }
 
-class Form extends React.Component {
+function getCharaData(lodestoneId, setCharacterData){ 
+  fetch(`https://xivapi.com/character/${lodestoneId}?data=CJ`).then(response => response.json()).then(jsonData => {
+    reflecteCharacterData(jsonData);
+    setCharacterData(jsonData);
+  });
+}
+
+function reflecteCharacterData(jsonData){
+  console.log(jsonData);
+}
+
+class Profile extends React.Component {
   constructor(){
     super();
-    this.state = {value: ""};
+    this.state = {
+      lodestoneId: "",
+      characterData: {
+        name: "キャラクター名",
+        imgSrc: "",
+
+      }
+    };
 
     this.handleChange = this.handleChange.bind(this);
+    this.setCharacterData = this.setCharacterData.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({lodestoneId: event.target.value});
   }
 
-  render(){
+  setCharacterData(data){
+    this.setState({characterData: {
+      name: data.Character.Name,
+      imgSrc: data.Character.Portrait,
+
+    }});
+  }
+
+  render() {
     return (
       <div>
+        <div>
         <label>
           Lodestone ID：
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text"
+            value={this.state.lodestoneId}
+            onChange={this.handleChange} 
+          />
         </label>
-        <Button value={this.state.value} />
+        <Button lodestoneId={this.state.lodestoneId} setCharacterData={this.setCharacterData} />
+        </div>
+        <div>{this.state.characterData.name}</div>
+        <div>
+          {/* {this.state.characterData.imgSrc && <img className="profileImage"
+          src={this.state.characterData.imgSrc} alt="プロフィール画像" />} */}
+          {this.state.characterData.imgSrc ? 
+          <img className="profileImage"　src={this.state.characterData.imgSrc} alt="プロフィール画像" />: 
+          <span>ここに画像が出ます</span>}
+        </div>
+        <div className="job">
+          <p>ジョブレベル</p>
+          <ul>TH</ul>
+          <ul>DPS</ul>
+          <ul>ギャザクラ</ul>
+        </div>
       </div>
-    );   
+    );
   }
 }
 
 
-function getCharaData(id){ 
-  fetch(`https://xivapi.com/character/${id}?data=CJ`).then(response => response.json()).then(jsonData => console.log(jsonData));
-}
-
 ReactDOM.render(
-  <Form />,
+  <Profile />,
   document.getElementById('root')
 );
