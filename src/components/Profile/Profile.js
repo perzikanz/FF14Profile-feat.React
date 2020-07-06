a1364ff2795f908b474be55dddc46d365ff05922
perzikanz <65535030+perzikanz@users.noreply.github.com>
2020-07-06 19:45:51 +0900

    jobの管理のリファクタリング

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,36 +3,7 @@ import './Profile.css';
 import SearchBox from '../SearchBox';
 import JobList from '../JobList';
 import { setCharacterImageSrc } from '../../redux/character/characterAction';
-
-import pld from '../../JobIcons/pld.png';
-import war from '../../JobIcons/war.png';
-import drk from '../../JobIcons/drk.png';
-import gnb from '../../JobIcons/gnb.png';
-import whm from '../../JobIcons/whm.png';
-import sch from '../../JobIcons/sch.png';
-import ast from '../../JobIcons/ast.png';
-import mnk from '../../JobIcons/mnk.png';
-import drg from '../../JobIcons/drg.png';
-import nin from '../../JobIcons/nin.png';
-import sam from '../../JobIcons/sam.png';
-import brd from '../../JobIcons/brd.png';
-import mcn from '../../JobIcons/mcn.png';
-import dnc from '../../JobIcons/dnc.png';
-import blm from '../../JobIcons/blm.png';
-import smn from '../../JobIcons/smn.png';
-import rdm from '../../JobIcons/rdm.png';
-import blu from '../../JobIcons/blu.png';
-import crp from '../../JobIcons/crp.png';
-import bsm from '../../JobIcons/bsm.png';
-import arm from '../../JobIcons/arm.png';
-import gsm from '../../JobIcons/gsm.png';
-import ltw from '../../JobIcons/ltw.png';
-import wvr from '../../JobIcons/wvr.png';
-import alc from '../../JobIcons/alc.png';
-import cul from '../../JobIcons/cul.png';
-import min from '../../JobIcons/min.png';
-import btn from '../../JobIcons/btn.png';
-import fsh from '../../JobIcons/fsh.png';
+import { JOB_TABLE } from '../../constant/job';
 
 import gla from '../../JobIcons/gla.png';
 import mrd from '../../JobIcons/mrd.png';
@@ -43,7 +14,6 @@ import rog from '../../JobIcons/rog.png';
 import arc from '../../JobIcons/arc.png';
 import thm from '../../JobIcons/thm.png';
 import acn from '../../JobIcons/acn.png';
-import { connect } from 'react-redux';
 
 const CLASS_TABLE = [
   { className: 'gla', classID: 1, img: gla },
@@ -57,38 +27,6 @@ const CLASS_TABLE = [
   { className: 'acn', classID: 26, img: acn },
 ];
 
-const JOB_TABLE = [
-  { jobName: 'pld', JobID: 19, img: pld },
-  { jobName: 'war', JobID: 21, img: war },
-  { jobName: 'drk', JobID: 32, img: drk },
-  { jobName: 'gnb', JobID: 37, img: gnb },
-  { jobName: 'whm', JobID: 24, img: whm },
-  { jobName: 'sch', JobID: 28, img: sch },
-  { jobName: 'ast', JobID: 33, img: ast },
-  { jobName: 'mnk', JobID: 20, img: mnk },
-  { jobName: 'drg', JobID: 22, img: drg },
-  { jobName: 'nin', JobID: 30, img: nin },
-  { jobName: 'sam', JobID: 34, img: sam },
-  { jobName: 'brd', JobID: 23, img: brd },
-  { jobName: 'mcn', JobID: 31, img: mcn },
-  { jobName: 'dnc', JobID: 38, img: dnc },
-  { jobName: 'blm', JobID: 25, img: blm },
-  { jobName: 'smn', JobID: 27, img: smn },
-  { jobName: 'rdm', JobID: 35, img: rdm },
-  { jobName: 'blu', JobID: 36, img: blu },
-  { jobName: 'crp', JobID: 8, img: crp },
-  { jobName: 'bsm', JobID: 9, img: bsm },
-  { jobName: 'arm', JobID: 10, img: arm },
-  { jobName: 'gsm', JobID: 11, img: gsm },
-  { jobName: 'ltw', JobID: 12, img: ltw },
-  { jobName: 'wvr', JobID: 13, img: wvr },
-  { jobName: 'alc', JobID: 14, img: alc },
-  { jobName: 'cul', JobID: 15, img: cul },
-  { jobName: 'min', JobID: 16, img: min },
-  { jobName: 'btn', JobID: 17, img: btn },
-  { jobName: 'fsh', JobID: 18, img: fsh },
-];
-
 export class Profile extends React.Component {
   constructor() {
     super();
@@ -97,35 +35,35 @@ export class Profile extends React.Component {
       characterData: {
         name: 'キャラクター名',
         job: [
-          { jobName: 'pld', JobID: 19, Level: '-', img: pld },
-          { jobName: 'war', JobID: 21, Level: '-', img: war },
-          { jobName: 'drk', JobID: 32, Level: '-', img: drk },
-          { jobName: 'gnb', JobID: 37, Level: '-', img: gnb },
-          { jobName: 'whm', JobID: 24, Level: '-', img: whm },
-          { jobName: 'sch', JobID: 28, Level: '-', img: sch },
-          { jobName: 'ast', JobID: 33, Level: '-', img: ast },
-          { jobName: 'mnk', JobID: 20, Level: '-', img: mnk },
-          { jobName: 'drg', JobID: 22, Level: '-', img: drg },
-          { jobName: 'nin', JobID: 30, Level: '-', img: nin },
-          { jobName: 'sam', JobID: 34, Level: '-', img: sam },
-          { jobName: 'brd', JobID: 23, Level: '-', img: brd },
-          { jobName: 'mcn', JobID: 31, Level: '-', img: mcn },
-          { jobName: 'dnc', JobID: 38, Level: '-', img: dnc },
-          { jobName: 'blm', JobID: 25, Level: '-', img: blm },
-          { jobName: 'smn', JobID: 27, Level: '-', img: smn },
-          { jobName: 'rdm', JobID: 35, Level: '-', img: rdm },
-          { jobName: 'blu', JobID: 36, Level: '-', img: blu },
-          { jobName: 'crp', JobID: 8, Level: '-', img: crp },
-          { jobName: 'bsm', JobID: 9, Level: '-', img: bsm },
-          { jobName: 'arm', JobID: 10, Level: '-', img: arm },
-          { jobName: 'gsm', JobID: 11, Level: '-', img: gsm },
-          { jobName: 'ltw', JobID: 12, Level: '-', img: ltw },
-          { jobName: 'wvr', JobID: 13, Level: '-', img: wvr },
-          { jobName: 'alc', JobID: 14, Level: '-', img: alc },
-          { jobName: 'cul', JobID: 15, Level: '-', img: cul },
-          { jobName: 'min', JobID: 16, Level: '-', img: min },
-          { jobName: 'btn', JobID: 17, Level: '-', img: btn },
-          { jobName: 'fsh', JobID: 18, Level: '-', img: fsh },
+          { JobID: 19, Level: '-' },
+          { JobID: 21, Level: '-' },
+          { JobID: 32, Level: '-' },
+          { JobID: 37, Level: '-' },
+          { JobID: 24, Level: '-' },
+          { JobID: 28, Level: '-' },
+          { JobID: 33, Level: '-' },
+          { JobID: 20, Level: '-' },
+          { JobID: 22, Level: '-' },
+          { JobID: 30, Level: '-' },
+          { JobID: 34, Level: '-' },
+          { JobID: 23, Level: '-' },
+          { JobID: 31, Level: '-' },
+          { JobID: 38, Level: '-' },
+          { JobID: 25, Level: '-' },
+          { JobID: 27, Level: '-' },
+          { JobID: 35, Level: '-' },
+          { JobID: 36, Level: '-' },
+          { JobID: 8, Level: '-' },
+          { JobID: 9, Level: '-' },
+          { JobID: 10, Level: '-' },
+          { JobID: 11, Level: '-' },
+          { JobID: 12, Level: '-' },
+          { JobID: 13, Level: '-' },
+          { JobID: 14, Level: '-' },
+          { JobID: 15, Level: '-' },
+          { JobID: 16, Level: '-' },
+          { JobID: 17, Level: '-' },
+          { JobID: 18, Level: '-' },
         ],
       },
       isReloading: false,
@@ -153,18 +91,18 @@ export class Profile extends React.Component {
         return fetchedJob.JobID === targetJobID;
       });
       const unID = targetJob.UnlockedState.ID;
+      const unName = targetJob.UnlockedState.Name;
       let imgSrc;
-      if (unID !== null && (unID < 8 || unID === 29 || unID === 26)) {
+
+      if (unName === 'Blue Mage (Limited Job)') {
+        imgSrc = JOB_TABLE[36].img;
+      } else if (unID < 8 || unID === 29 || unID === 26) {
         const targetClass = CLASS_TABLE.find((classe) => {
           return classe.classID === unID;
         });
         imgSrc = targetClass.img;
-      } else if (unID === null) {
-        imgSrc = blu;
       } else {
-        const targetJob = JOB_TABLE.find((job) => {
-          return job.JobID === unID;
-        });
+        const targetJob = JOB_TABLE[unID];
         imgSrc = targetJob.img;
       }
       return {
---
+++ src/constant/job.js
@@ -0,0 +1,148 @@
+import pld from '../JobIcons/pld.png';
+import war from '../JobIcons/war.png';
+import drk from '../JobIcons/drk.png';
+import gnb from '../JobIcons/gnb.png';
+import whm from '../JobIcons/whm.png';
+import sch from '../JobIcons/sch.png';
+import ast from '../JobIcons/ast.png';
+import mnk from '../JobIcons/mnk.png';
+import drg from '../JobIcons/drg.png';
+import nin from '../JobIcons/nin.png';
+import sam from '../JobIcons/sam.png';
+import brd from '../JobIcons/brd.png';
+import mcn from '../JobIcons/mcn.png';
+import dnc from '../JobIcons/dnc.png';
+import blm from '../JobIcons/blm.png';
+import smn from '../JobIcons/smn.png';
+import rdm from '../JobIcons/rdm.png';
+import blu from '../JobIcons/blu.png';
+import crp from '../JobIcons/crp.png';
+import bsm from '../JobIcons/bsm.png';
+import arm from '../JobIcons/arm.png';
+import gsm from '../JobIcons/gsm.png';
+import ltw from '../JobIcons/ltw.png';
+import wvr from '../JobIcons/wvr.png';
+import alc from '../JobIcons/alc.png';
+import cul from '../JobIcons/cul.png';
+import min from '../JobIcons/min.png';
+import btn from '../JobIcons/btn.png';
+import fsh from '../JobIcons/fsh.png';
+
+export const JOB_TABLE = {
+  19: {
+    jobName: 'pld',
+    img: pld,
+  },
+  21: {
+    jobName: 'war',
+    img: war,
+  },
+  32: {
+    jobName: 'drk',
+    img: drk,
+  },
+  37: {
+    jobName: 'gnb',
+    img: gnb,
+  },
+  24: {
+    jobName: 'whm',
+    img: whm,
+  },
+  28: {
+    jobName: 'sch',
+    img: sch,
+  },
+  33: {
+    jobName: 'ast',
+    img: ast,
+  },
+  20: {
+    jobName: 'mnk',
+    img: mnk,
+  },
+  22: {
+    jobName: 'drg',
+    img: drg,
+  },
+  30: {
+    jobName: 'nin',
+    img: nin,
+  },
+  34: {
+    jobName: 'sam',
+    img: sam,
+  },
+  23: {
+    jobName: 'brd',
+    img: brd,
+  },
+  31: {
+    jobName: 'mcn',
+    img: mcn,
+  },
+  38: {
+    jobName: 'dnc',
+    img: dnc,
+  },
+  25: {
+    jobName: 'blm',
+    img: blm,
+  },
+  27: {
+    jobName: 'smn',
+    img: smn,
+  },
+  35: {
+    jobName: 'rdm',
+    img: rdm,
+  },
+  36: {
+    jobName: 'blu',
+    img: blu,
+  },
+  8: {
+    jobName: 'crp',
+    img: crp,
+  },
+  9: {
+    jobName: 'bsm',
+    img: bsm,
+  },
+  10: {
+    jobName: 'arm',
+    img: arm,
+  },
+  11: {
+    jobName: 'gsm',
+    img: gsm,
+  },
+  12: {
+    jobName: 'ltw',
+    img: ltw,
+  },
+  13: {
+    jobName: 'wvr',
+    img: wvr,
+  },
+  14: {
+    jobName: 'alc',
+    img: alc,
+  },
+  15: {
+    jobName: 'cul',
+    img: cul,
+  },
+  16: {
+    jobName: 'min',
+    img: min,
+  },
+  17: {
+    jobName: 'btn',
+    img: btn,
+  },
+  18: {
+    jobName: 'fsh',
+    img: fsh,
+  },
+};
